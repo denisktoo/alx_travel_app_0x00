8dbf8b58639051aa19f42ed3cf42c1d795a53745
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-07-17 17:23:20 +0300

    Create Models and Set Up Serializers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -6,7 +6,7 @@ class ListingSerializer(serializers.HyperlinkedModelSerializer):
         model = Listing
         fields = ['url', 'property_id', 'host', 'name', 'description', 'location', 'price_per_night', 'created_at', 'updated_at']
 
-class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+class BookingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Booking
-        fields = ['url', 'booking_id', 'property', 'user', 'start_date', 'end_date' 'total_price', 'status', 'created_at']
+        fields = ['url', 'booking_id', 'property', 'user', 'start_date', 'end_date', 'total_price', 'status', 'created_at']
