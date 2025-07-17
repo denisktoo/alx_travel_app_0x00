eb9daa19bee12a4c40ce87e5bd5254f15fa2d655
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-07-17 19:26:09 +0300

    Set Up Serializers

--- alx_travel_app/listings/management/commands/seed.py
+++ alx_travel_app/listings/management/commands/seed.py
@@ -1,3 +1,4 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from listings.models import Listing, Review, Booking
+
--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -2,11 +2,27 @@ from rest_framework import serializers
 from .models import Listing, Booking
 
 class ListingSerializer(serializers.HyperlinkedModelSerializer):
+    host_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Listing
-        fields = ['url', 'property_id', 'host', 'name', 'description', 'location', 'price_per_night', 'created_at', 'updated_at']
+        fields = ['url', 'property_id', 'host', 'host_name', 'name', 'description', 'location', 'price_per_night', 'created_at', 'updated_at']
+
+        def get_host_name(self, obj):
+            return f"{obj.host.first_name} {obj.host.last_name}"
 
 class BookingSerializer(serializers.HyperlinkedModelSerializer):
+    status = serializers.CharField()
+
     class Meta:
         model = Booking
         fields = ['url', 'booking_id', 'property', 'user', 'start_date', 'end_date', 'total_price', 'status', 'created_at']
+
+        def validate_status(self, value):
+            """
+            Use serializers.ValidationError to validate status field.
+            """
+            allowed_statuses = ['confirmed', 'pending', 'cancelled']
+            if value.lower() not in allowed_statuses:
+                raise serializers.ValidationError(f"Status must be one of {allowed_statuses}.")
+            return value
