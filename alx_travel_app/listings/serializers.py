d8bd75ca19c030356183119e80746ca1502533b4
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-07-17 19:37:41 +0300

    Set Up Serializers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -8,8 +8,8 @@ class ListingSerializer(serializers.HyperlinkedModelSerializer):
         model = Listing
         fields = ['url', 'property_id', 'host', 'host_name', 'name', 'description', 'location', 'price_per_night', 'created_at', 'updated_at']
 
-        def get_host_name(self, obj):
-            return f"{obj.host.first_name} {obj.host.last_name}"
+    def get_host_name(self, obj):
+        return f"{obj.host.first_name} {obj.host.last_name}"
 
 class BookingSerializer(serializers.HyperlinkedModelSerializer):
     status = serializers.CharField()
@@ -18,11 +18,11 @@ class BookingSerializer(serializers.HyperlinkedModelSerializer):
         model = Booking
         fields = ['url', 'booking_id', 'property', 'user', 'start_date', 'end_date', 'total_price', 'status', 'created_at']
 
-        def validate_status(self, value):
-            """
-            Use serializers.ValidationError to validate status field.
-            """
-            allowed_statuses = ['confirmed', 'pending', 'cancelled']
-            if value.lower() not in allowed_statuses:
-                raise serializers.ValidationError(f"Status must be one of {allowed_statuses}.")
-            return value
+    def validate_status(self, value):
+        """
+        Use serializers.ValidationError to validate status field.
+        """
+        allowed_statuses = ['confirmed', 'pending', 'cancelled']
+        if value.lower() not in allowed_statuses:
+            raise serializers.ValidationError(f"Status must be one of {allowed_statuses}.")
+        return value
