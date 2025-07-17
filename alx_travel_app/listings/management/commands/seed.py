8d42c8e236f39dd2fd80573435d2b7983374f4d8
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-07-17 19:33:07 +0300

    Seeders

--- alx_travel_app/listings/management/commands/seed.py
+++ alx_travel_app/listings/management/commands/seed.py
@@ -2,3 +2,10 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from listings.models import Listing, Review, Booking
 
+class Command(BaseCommand):
+    help = "Seed the database with sample Users, Listings, Reviews, and Bookings"
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS("Seeding database…"))
+
+        self.stdout.write(self.style.SUCCESS("Database seeded successfully!"))
